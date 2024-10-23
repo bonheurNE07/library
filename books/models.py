7e19c2a4c4cbcf840b9b9abd29f1d76f0e28f007
bonheurNE07 <bonheurndezenc@gmail.com>
2024-10-23 17:13:19 +0200

    Book model creation for the books app

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model)
+    title = models.CharField(max_length=255)
+    author_name = models.CharField(max_length=255)
+    published_date = models.DateField()
+    language = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
+    
