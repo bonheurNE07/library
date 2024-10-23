4a52aaff60680e00ffeebf0cd5fbc935b230ebeb
bonheurNE07 <bonheurndezenc@gmail.com>
2024-10-23 18:23:08 +0200

    object-level vlidation in serializer done

--- books/serializers.py
+++ books/serializers.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from rest_framework import serializers
 from .models import Book
 
@@ -8,10 +10,25 @@ class BookSerializer(serializers.ModelSerializer):
 
         fields = ['id','title','author_name', 'published_date', 'language']
         read_only_fields = ['id','published_date']
-        
+    
+    # exemple of field-level validation
     def validate_title(self, value):
         if value == "":
             raise serializers.ValidationError("Title cannot be Empty.")
         elif value == "Untitled":
             raise serializers.ValidationError("Title cannot be 'Untitled'.")
         return value
+    
+    # example of object-level validation
+    def validate(self, data):
+        """
+        This is used only to validate the entire object (multiple fields together)
+        """
+        if data['published_date'] > date.today():
+            raise serializers.ValidationError("Published date cannot be in the future")
+        if data['author_name'].isdigit():
+            raise serializers.ValidationError("Authors name cannot be digits.")
+        if data['language'].isdigit():
+            raise serializers.ValidationError("Books language cannot be digits.")
+        
+        return data
