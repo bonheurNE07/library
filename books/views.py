8aa15153050d6a906dfd975a2fa474cec72d7802
bonheurNE07 <bonheurndezenc@gmail.com>
2024-10-23 18:30:12 +0200

    definition of the view for listing and creating books

--- books/views.py
+++ books/views.py
@@ -2,3 +2,10 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
+class BookListCreateView(generics.ListCreateAPIView):
+    """ ListCreateAPIView requires two mandatory attributes, serializer_class and queryset """
+
+    # We specify BookSerializer which we have early implemented in the serializers module
+    queryset = Book.objects.all()
+    serilizer_class = BookSerializer
+    
