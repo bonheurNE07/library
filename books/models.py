471c3cc3d89bf95e3fc1edbb4531a237581f394b
bonheurNE07 <bonheurndezenc@gmail.com>
2024-10-23 17:39:53 +0200

    BookSerializer class implementations and fields specification

--- books/models.py
+++ books/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Book(models.Model)
+class Book(models.Model):
     title = models.CharField(max_length=255)
     author_name = models.CharField(max_length=255)
     published_date = models.DateField()
