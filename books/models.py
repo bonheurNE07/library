1f83a0184be7db2fc9488c9b309f7af71abf9c99
bonheurNE07 <bonheurndezenc@gmail.com>
2024-10-23 19:20:28 +0200

    models modification , to set a default now date as value of the published_date field

--- books/models.py
+++ books/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author_name = models.CharField(max_length=255)
-    published_date = models.DateField()
+    published_date = models.DateField(default=timezone.now)
     language = models.CharField(max_length=150)
 
     def __str__(self):
